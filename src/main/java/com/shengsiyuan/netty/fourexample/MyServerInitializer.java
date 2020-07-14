ec8a229179e67e8604dc8345b51da20a1cb689b0
xuqingxin <xuqingxin@didiglobal.com>
2020-07-14 15:33:51 +0800

    netty

---
+++ src/main/java/com/shengsiyuan/netty/fourexample/MyServerInitializer.java
@@ -0,0 +1,25 @@
+package com.shengsiyuan.netty.fourexample;
+
+import com.shengsiyuan.netty.thirdexample.MyChatServerHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new IdleStateHandler(5, 7, 10, TimeUnit.SECONDS));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
---
+++ src/main/java/com/shengsiyuan/netty/thirdexample/MyChatClientHandler.java
@@ -0,0 +1,24 @@
+package com.shengsiyuan.netty.thirdexample;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class MyChatClientHandler extends SimpleChannelInboundHandler<String> {
+
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+
+        System.out.println(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
